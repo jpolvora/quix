3d47e5aadb2ad25e1fe6cfc8fea8479d82627a63
JONE POLVORA <jpolvora@gmail.com>
2023-10-26 19:05:27 -0400

    fix npm start

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
 # Version 4
 FROM node:18-alpine AS builder
 WORKDIR /app
-COPY ["package*.json", "tsconfig.json", "tsconfig-build.json", "app-*.json", "./"]
+COPY ["package*.json", "tsconfig*.json", "./"]
 RUN npm ci
-COPY ./src ./src
+COPY ./dist .
 RUN npm run build
 
 FROM node:18-alpine AS final
 WORKDIR /app
 COPY --from=builder ./app/dist ./
-COPY ["package*.json", "app-*.json", "./"]
+COPY ["package*.json", "tsconfig*.json", "./"]
 RUN npm install --production
 RUN npm install pm2 -g
 # RUN pm2 update
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,19 @@
 module.exports = {
     apps: [
         {
-            name: "quix",
+            name: "app-proxy",
+            script: "./dist/app-proxy/index.js",            
+            exec_mode: "cluster",
+            instances: Number(process.env.INSTANCES) || 2,
+            max_memory_restart: "512M",
+            wait_ready: true,
+            env: {
+                NODE_ENV: "production",
+                PORT: 3000
+            },
+        },
+        {
+            name: "app-account",
             script: "./dist/index.js",
             args: "-m app-account",
             exec_mode: "cluster",
@@ -10,6 +22,20 @@ module.exports = {
             wait_ready: true,
             env: {
                 NODE_ENV: "production",
+                PORT: 3001
+            },
+        },
+        {
+            name: "app-transactions",
+            script: "./dist/index.js",
+            args: "-m app-transactions",
+            exec_mode: "cluster",
+            instances: Number(process.env.INSTANCES) || 2,
+            max_memory_restart: "512M",
+            wait_ready: true,
+            env: {
+                NODE_ENV: "production",
+                PORT: 3002
             },
         },
     ],
