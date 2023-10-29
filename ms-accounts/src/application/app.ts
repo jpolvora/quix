6446f5a198cfa5111a5b1c76003096ee0db2cdd2
JONE POLVORA <jpolvora@gmail.com>
2023-10-29 01:39:11 -0400

    added http error handling 400/500 tests

--- ms-accounts/src/application/app.ts
+++ ms-accounts/src/application/app.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response, json } from 'express'
 import { AppController } from './AppController'
 import { NextFunction } from 'connect'
+import { ValidationError } from '@/validation/ValidationError'
+import { DbError } from '@/validation/DbError'
 
 export async function setupApp(): Promise<Express> {
   //ensure services connected before proceed ()
@@ -36,16 +38,36 @@ export async function setupApp(): Promise<Express> {
     })
   })
 
+  app.get('/throw500', (req, res) => {
+    var error = new Error('Error forçado')
+    throw error
+  })
+
+  app.get('/throw400', (req, res) => {
+    var error = new ValidationError('Erro forçado')
+    throw error
+  })
+
   await new AppController(app).configureRoutes()
 
   app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
     console.error(err.stack)
 
+    let statusCode = 500
+    if (err instanceof ValidationError) statusCode = 400
+    if (err instanceof DbError) statusCode = 500
+
     return res.status(500).json({
       success: false,
-      statusCode: 500,
+      env: process.env.NODE_ENV,
+      statusCode: statusCode,
       error: err,
-      errorDetails: err.message,
+      errorDetails:
+        (process.env.NODE_ENV !== 'production' && {
+          message: err.message,
+          stack: err.stack,
+        }) ||
+        undefined,
     })
   })
   return app
